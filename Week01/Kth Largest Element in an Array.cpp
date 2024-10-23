2335cee39812306feba8b40566ece5ced92abdaa
Mehedi Hasan Akash <85875301+m-akash@users.noreply.github.com>
2024-10-23 22:43:04 +0600

    Update Kth Largest Element in an Array.cpp

--- Week01/Kth Largest Element in an Array.cpp
+++ Week01/Kth Largest Element in an Array.cpp
@@ -1,13 +1,40 @@
+mt19937 rng(0);
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> MinHeap;
-        for(auto it : nums) {
-            MinHeap.push(it);
-            if(MinHeap.size() > k) {
-                MinHeap.pop();
+    int partition(vector<int> &arr, int L, int R) {
+        int pivot = arr[R];
+        int i = L - 1;
+        for(int j = L; j < R; j++) {
+            if(arr[j] <= pivot) {
+                i++;
+                swap(arr[i], arr[j]);
             }
         }
-        return MinHeap.top();
+        swap(arr[i + 1], arr[R]);
+        return i + 1;
+    }
+    void partial_sort(vector<int> & arr, int k) {
+        shuffle(arr.begin(), arr.end(), rng);
+        int L = 0; int R = arr.size() - 1;
+        int threshold = 2 * log2(arr.size());
+        while(L < R and threshold--) {
+            int p = partition(arr, L, R);
+            if(p < k) {
+                L = p + 1;
+            }
+            else if(p > k) {
+                R = p - 1;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    int findKthSmallest(vector<int>& arr, int k) {
+        partial_sort(arr, k);
+        return arr[k];
+    }
+    int findKthLargest(vector<int>& arr, int k) {
+        return findKthSmallest(arr, arr.size() - k);
     }
 };
