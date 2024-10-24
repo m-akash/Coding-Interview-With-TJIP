71df27f0f3b06ef66430407eed350ca8fc0a3bf3
Mehedi Hasan Akash <85875301+m-akash@users.noreply.github.com>
2024-10-24 22:55:49 +0600

    Update Majority Element.cpp

--- Week01/Majority Element.cpp
+++ Week01/Majority Element.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size(); int MajorityElement;
-        map<int, int> CountMajority;
+        int n = nums.size(); 
+        int cnt = 0; int element;
         for(int i = 0; i < n; i++) {
-            CountMajority[nums[i]]++;
-        }
-        for(auto it : CountMajority) {
-            if(it.second > n/2){
-                MajorityElement = it.first;
+            if(cnt == 0) {
+                cnt = 1;
+                element = nums[i];
+            }
+            else if(element == nums[i]) {
+                cnt++;
+            }
+            else {
+                cnt--;
             }
         }
-        return MajorityElement;
+        int appears = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == element) {
+                appears++;
+            }
+        }
+        if(appears > n/2) {
+            return element;
+        }
+        return -1;
     }
 };
