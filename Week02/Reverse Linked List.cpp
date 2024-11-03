3b185f2acec537eb392b523e3ee5a99712887bf6
Mehedi Hasan Akash <85875301+m-akash@users.noreply.github.com>
2024-11-03 20:25:42 +0600

    Create Reverse Linked List.cpp

---
+++ Week02/Reverse Linked List.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* current = head;
+        ListNode* previous = nullptr;
+        while (current) {
+            auto next_ref = current->next;
+            current->next = previous;
+            previous = current;
+            current = next_ref;
+        }
+        return previous;
+    }
+};
