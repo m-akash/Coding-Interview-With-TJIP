5e0d37d08bbc793a1d5291adeaef686280ddf507
Mehedi Hasan Akash <85875301+m-akash@users.noreply.github.com>
2025-01-13 19:42:47 +0600

    Update Binary Tree Vertical Order Traversal.cpp

--- Week06/Binary Tree Vertical Order Traversal.cpp
+++ Week06/Binary Tree Vertical Order Traversal.cpp
@@ -1,7 +1,14 @@
+/*
+Complexity Analysis
+Time Complexity: O(V.logN)
+Space Complexity: O(V)
+*/
 class Solution {  
 public:
     vector<vector<int>> verticalOrder(TreeNode* root) {
-        if(root == nullptr)return {};
+        if(root == nullptr) {
+            return {};
+        }
         map<int, vector<int>>values;
         queue<pair<TreeNode*, int>>Q;
         Q.push({root, 0});
@@ -9,11 +16,17 @@ public:
             auto [u, cand] = Q.front();
             Q.pop();
             values[cand].push_back(u->val);
-            if(u->left != nullptr) Q.push({u->left, cand - 1});
-            if(u->right != nullptr) Q.push({u->right, cand + 1});
+            if(u->left != nullptr) { 
+                Q.push({u->left, cand - 1});
+            }
+            if(u->right != nullptr) { 
+                Q.push({u->right, cand + 1});
+            }
         }
         vector<vector<int>>ans;
-        for(auto &it:values)ans.push_back(it.second);
+        for(auto &it:values) { 
+            ans.push_back(it.second);
+        }
         return ans;
     }
 };
